9ecbe84a615fc78b2c8f01a0a406a07d98169148
Fabian Verdugo <verdugofabian97@gmail.com>
2022-07-15 01:33:16 -0400

    Configuracion de correo

--- snkshop/core/settings.py
+++ snkshop/core/settings.py
@@ -188,8 +188,15 @@ AUTHENTICATION_BACKENDS = (
 
 
 
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 
-
-
+if not DEBUG:
+    DEFAULT_FROM_EMAIL = 'SNKSHOP - Tienda de Snekears <pipelemus55@gmail.com>'
+    EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = env('EMAIL_HOST')
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    EMAIL_PORT = env('EMAIL_PORT')
+    EMAIL_USE_TLS = env('EMAIL_USE_TLS')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
